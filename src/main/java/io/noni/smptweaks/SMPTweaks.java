6e0999fb001b6e4c0563751e9313a233c7fdbe79
noniDOTio <Good!fri3ndly>
2020-10-08 22:40:09 +0200

    Initial commit

---
+++ src/main/java/io/noni/smptweaks/SMPTweaks.java
@@ -0,0 +1,17 @@
+package io.noni.smptweaks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPTweaks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
