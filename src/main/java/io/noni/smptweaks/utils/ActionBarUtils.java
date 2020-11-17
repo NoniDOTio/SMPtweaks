42395a163dd317604658e4068bdc48f6be682d4f
noniDOTio <noni-online@outlook.de>
2020-11-18 00:01:06 +0100

    add ActionBar helper class

---
+++ src/main/java/io/noni/smptweaks/utils/ActionBarUtils.java
@@ -0,0 +1,28 @@
+package io.noni.smptweaks.utils;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.entity.Player;
+
+public class ActionBarUtils {
+
+
+    /**
+     * Send a positive or neutral action bar message to a player
+     * @param player
+     * @param message
+     */
+    public static void notify(Player player, String message) {
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
+    }
+
+
+    /**
+     * Send a negative action bar message to a player
+     * @param player
+     * @param message
+     */
+    public static void negativeNotify(Player player, String message) {
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
+    }
+}
