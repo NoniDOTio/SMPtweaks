544a3c063046ca150822e508d45da9fe2339da7d
noniDOTio <noni-online@outlook.de>
2021-01-01 23:55:37 +0100

    Move PDCKey class to seperate file

---
+++ src/main/java/io/noni/smptweaks/models/PDCKey.java
@@ -0,0 +1,35 @@
+package io.noni.smptweaks.models;
+
+import io.noni.smptweaks.SMPTweaks;
+import org.bukkit.NamespacedKey;
+import org.bukkit.persistence.PersistentDataType;
+
+public class PDCKey<T> {
+    public static final PDCKey<Integer> LEVEL;
+    public static final PDCKey<Integer> TOTAL_XP;
+    public static final PDCKey<Integer> XP_DISPLAY_MODE;
+    public static final PDCKey<Integer> SPECIAL_DROP_AVAILABLE;
+
+    static {
+        LEVEL = new PDCKey("level", PersistentDataType.INTEGER);
+        TOTAL_XP = new PDCKey("total_xp", PersistentDataType.INTEGER);
+        XP_DISPLAY_MODE = new PDCKey("xp_display_mode", PersistentDataType.INTEGER);
+        SPECIAL_DROP_AVAILABLE = new PDCKey("special_drop_available", PersistentDataType.INTEGER);
+    }
+
+    public final NamespacedKey namespace;
+    public final PersistentDataType<T, T> type;
+
+    private PDCKey(String namespace, PersistentDataType<T, T> type) {
+        this.namespace = new NamespacedKey(SMPTweaks.getPlugin(), namespace);
+        this.type = type;
+    }
+
+    public NamespacedKey getKey() {
+        return namespace;
+    }
+
+    public PersistentDataType<T, T> getType() {
+        return type;
+    }
+}
--- src/main/java/io/noni/smptweaks/utils/PDCUtils.java
+++ src/main/java/io/noni/smptweaks/utils/PDCUtils.java
@@ -1,9 +1,7 @@
 package io.noni.smptweaks.utils;
 
-import io.noni.smptweaks.SMPTweaks;
-import org.bukkit.NamespacedKey;
+import io.noni.smptweaks.models.PDCKey;
 import org.bukkit.persistence.PersistentDataHolder;
-import org.bukkit.persistence.PersistentDataType;
 
 public class PDCUtils {
 
@@ -11,41 +9,12 @@ public class PDCUtils {
         throw new AssertionError("This utility class cannot be instantiated");
     }
 
-    public static <T> T get(PersistentDataHolder holder, Key<T> key) {
+    public static <T> T get(PersistentDataHolder holder, PDCKey<T> key) {
         return holder.getPersistentDataContainer().get(key.namespace, key.type);
     }
 
-    public static <T> void set(PersistentDataHolder holder, Key<T> key, T value) {
+    public static <T> void set(PersistentDataHolder holder, PDCKey<T> key, T value) {
         holder.getPersistentDataContainer().set(key.namespace, key.type, value);
     }
 
-    public static class Key<T> {
-        public static final Key<Integer> LEVEL;
-        public static final Key<Integer> TOTAL_XP;
-        public static final Key<Integer> XP_DISPLAY_MODE;
-        public static final Key<Integer> SPECIAL_DROP_AVAILABLE;
-
-        static {
-            LEVEL = new Key("level", PersistentDataType.INTEGER);
-            TOTAL_XP = new Key("total_xp", PersistentDataType.INTEGER);
-            XP_DISPLAY_MODE = new Key("xp_display_mode", PersistentDataType.INTEGER);
-            SPECIAL_DROP_AVAILABLE = new Key("special_drop_available", PersistentDataType.INTEGER);
-        }
-
-        private final NamespacedKey namespace;
-        private final PersistentDataType<T, T> type;
-
-        private Key(String namespace, PersistentDataType<T, T> type) {
-            this.namespace = new NamespacedKey(SMPTweaks.getPlugin(), namespace);
-            this.type = type;
-        }
-
-        public NamespacedKey getKey() {
-            return namespace;
-        }
-
-        public PersistentDataType<T, T> getType() {
-            return type;
-        }
-    }
 }
