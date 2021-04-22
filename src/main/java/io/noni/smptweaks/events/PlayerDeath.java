e232f4521df6d8c77de8b71404d3abe18b1629cd
noniDOTio <noni-online@outlook.de>
2021-04-22 23:47:16 +0200

    Add option to decrease durability of all items on PlayerDeath

--- src/main/java/io/noni/smptweaks/events/PlayerDeath.java
+++ src/main/java/io/noni/smptweaks/events/PlayerDeath.java
@@ -1,10 +1,12 @@
 package io.noni.smptweaks.events;
 
 import io.noni.smptweaks.SMPTweaks;
+import io.noni.smptweaks.utils.ChatUtils;
+import io.noni.smptweaks.utils.ExperienceUtils;
 import io.noni.smptweaks.utils.LoggingUtils;
-import org.bukkit.ChatColor;
 import org.bukkit.GameRule;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.entity.Player;
@@ -12,6 +14,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
+
 
 public class PlayerDeath implements Listener {
 
@@ -19,8 +24,21 @@ public class PlayerDeath implements Listener {
     public void onPlayerDeath(PlayerDeathEvent e) {
         Player player = e.getEntity().getPlayer();
 
+        /**
+         * Alter durability of items
+         */
+        if(SMPTweaks.getCfg().getBoolean("decrease_item_durability_on_death.enabled")) {
+            double durabilityMultiplier = SMPTweaks.getCfg().getDouble("decrease_item_durability_on_death.multiplier");
+            for (ItemStack itemStack : player.getInventory().getContents()) {
+                multiplyItemDurability(itemStack, durabilityMultiplier);
+            }
+            for (ItemStack itemStack : player.getInventory().getArmorContents()) {
+                multiplyItemDurability(itemStack, durabilityMultiplier);
+            }
+        }
+
         if(!player.getWorld().getGameRuleValue(GameRule.KEEP_INVENTORY)) {
-            LoggingUtils.warn("Skipping PlayerDeath logic. You need to set keepInventory to TRUE in order to use this feature.");
+            LoggingUtils.warn("Skipping remaining PlayerDeath logic. You need to set keepInventory to TRUE in order to use this feature.");
             return;
         }
 
@@ -28,7 +46,7 @@ public class PlayerDeath implements Listener {
          * Remove XP from player and drop on ground
          */
         if(SMPTweaks.getCfg().getBoolean("remove_xp_on_death.enabled")) {
-            int currentXp = player.getTotalExperience();
+            int currentXp = ExperienceUtils.getTotalExperience(player);
 
             // Calculate how much to drop
             int baseXpToRemove = SMPTweaks.getCfg().getInt("remove_xp_on_death.base");
@@ -39,12 +57,17 @@ public class PlayerDeath implements Listener {
             int xpToRemove = Math.min(resultXpToRemove, SMPTweaks.getCfg().getInt("remove_xp_on_death.max"));
 
             // Remove XP from player
-            player.setTotalExperience(Math.max(0, currentXp - xpToRemove));
+            ExperienceUtils.setTotalExperience(player, Math.max(0, currentXp - xpToRemove));
 
             // Spawn XP orbs on the ground
             if(SMPTweaks.getCfg().getBoolean("remove_xp_on_death.drop_on_ground")) {
-                double xpDropMultiplier = SMPTweaks.getCfg().getDouble("remove_xp_on_death.drop_multiplier");
-                spawnOrbs((int) Math.floor(xpToRemove * xpDropMultiplier), 100, player);
+                double xpDropMultiplier = SMPTweaks.getCfg().getDouble("remove_xp_on_death.drop_amount_multiplier");
+                spawnOrbs(
+                        (int) Math.floor(xpToRemove * xpDropMultiplier),
+                        Math.max(50, (int) xpToRemove / 10),
+                        player.getWorld(),
+                        player.getLocation()
+                );
             }
         }
 
@@ -59,22 +82,27 @@ public class PlayerDeath implements Listener {
                     "remove_inventory_on_death.include_non_stackable_items"
             ) ? 0 : 1;
             boolean inventoryDropOnGround = SMPTweaks.getCfg().getBoolean("remove_inventory_on_death.drop_on_ground");
-            double inventoryDropMultiplier = SMPTweaks.getCfg().getDouble("remove_inventory_on_death.drop_multiplier");
+            double inventoryDropMultiplier = SMPTweaks.getCfg().getDouble("remove_inventory_on_death.drop_amount_multiplier");
 
             for (ItemStack itemStack : player.getInventory().getContents()) {
                 if (itemStack != null && Math.random() < inventoryChancePerSlot) {
                     int itemStackSize = itemStack.getAmount();
 
-                    if(itemStackSize > minAffectedStackSize) {
-                        double portionToDrop = itemStackSize * inventoryPortionPerSlot;
-                        int amountToRemove = inventoryRandomize ? randomize(portionToDrop, 0.6) : (int) portionToDrop;
+                    if(minAffectedStackSize < itemStack.getMaxStackSize()) {
+                        double portionToRemove = itemStackSize * inventoryPortionPerSlot;
+                        int amountToRemove = inventoryRandomize ? (int) Math.ceil(randomize(portionToRemove, 0.6)) : (int) portionToRemove;
                         int amountToKeep = itemStackSize - amountToRemove;
+                        int amountToDrop = (int) Math.round(amountToRemove * inventoryDropMultiplier);
 
-                        itemStack.setAmount(amountToKeep);
+                        if(amountToKeep < 1) {
+                            player.getInventory().remove(itemStack);
+                        } else {
+                            itemStack.setAmount(amountToKeep);
+                        }
+                        ItemStack itemStackToDrop = itemStack.clone();
+                        itemStackToDrop.setAmount(amountToDrop);
 
-                        if(inventoryDropOnGround) {
-                            ItemStack itemStackToDrop = itemStack.clone();
-                            itemStackToDrop.setAmount((int) Math.floor(amountToRemove * inventoryDropMultiplier));
+                        if(inventoryDropOnGround && !itemStack.getType().isAir() && amountToDrop > 0) {
                             player.getWorld().dropItemNaturally(player.getLocation(), itemStackToDrop);
                         }
                     }
@@ -88,39 +116,108 @@ public class PlayerDeath implements Listener {
         if(SMPTweaks.getCfg().getBoolean("remove_equipment_on_death.enabled")) {
             double equipmentChancePerSlot = SMPTweaks.getCfg().getDouble("remove_equipment_on_death.chance_per_slot");
             boolean equipmentDropOnGround = SMPTweaks.getCfg().getBoolean("remove_equipment_on_death.drop_on_ground");
-            ItemStack[] armorContentsBefore = player.getInventory().getArmorContents();
-            ItemStack[] armorContentsAfter = new ItemStack[4];
 
-            int i = 0;
-            for(ItemStack stack : armorContentsBefore) {
-                if(stack != null && Math.random() < equipmentChancePerSlot) {
-                    if(equipmentDropOnGround) {
-                        player.getWorld().dropItemNaturally(player.getLocation(), stack);
-                    }
-                    armorContentsAfter[i] = null;
-                } else {
-                    armorContentsAfter[i] = stack;
+            ItemStack helmet = player.getInventory().getHelmet();
+            ItemStack chestplate = player.getInventory().getChestplate();
+            ItemStack leggings = player.getInventory().getLeggings();
+            ItemStack boots = player.getInventory().getBoots();
+
+            // Helmet
+            if(helmet != null && Math.random() < equipmentChancePerSlot) {
+                player.getInventory().setHelmet(null);
+                if(equipmentDropOnGround) {
+                    player.getWorld().dropItemNaturally(player.getLocation(), helmet);
                 }
-                i++;
             }
-            player.getInventory().setArmorContents(armorContentsAfter);
+
+            // Chestplate
+            if(chestplate != null && Math.random() < equipmentChancePerSlot) {
+                player.getInventory().setChestplate(null);
+                if(equipmentDropOnGround) {
+                    player.getWorld().dropItemNaturally(player.getLocation(), chestplate);
+                }
+            }
+
+            // Leggings
+            if(leggings != null && Math.random() < equipmentChancePerSlot) {
+                player.getInventory().setLeggings(null);
+                if(equipmentDropOnGround) {
+                    player.getWorld().dropItemNaturally(player.getLocation(), leggings);
+                }
+            }
+
+            // Boots
+            if(boots != null && Math.random() < equipmentChancePerSlot) {
+                player.getInventory().setBoots(null);
+                if(equipmentDropOnGround) {
+                    player.getWorld().dropItemNaturally(player.getLocation(), boots);
+                }
+            }
         }
 
         /**
          * Send chat message to dead player
          */
-        player.sendMessage(ChatColor.RED + "Du hast einen Teil deiner Gegenstände und Erfahrung verloren :(");
+        ChatUtils.negative(player, "Du hast einen Teil deiner Gegenstände und Erfahrung verloren :(");
     }
 
+    /**
+     *
+     * @param amount
+     * @param deviation
+     * @return
+     */
     private int randomize(double amount, double deviation) {
-        double multiplier = Math.random() * (2 * deviation) - deviation;
-        return (int) (amount * multiplier);
+        double multiplier = 1 + (Math.random() * 2 * deviation) - deviation;
+        return (int) Math.round(amount * multiplier);
     }
 
-    private void spawnOrbs(int xp, int orbSize, Player player) {
+    /**
+     *
+     * @param itemStack
+     * @param multiplier
+     * @return
+     */
+    private ItemStack multiplyItemDurability(ItemStack itemStack, double multiplier) {
+
+        if(itemStack == null) {
+            return null;
+        }
+
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if(itemMeta instanceof Damageable == false) {
+            return itemStack;
+        }
+
+        // Apply multiplier logic
+        int maximumDurability = itemStack.getType().getMaxDurability();
+        Damageable damageableItemMeta = (Damageable) itemMeta;
+        int currentDurability = maximumDurability - damageableItemMeta.getDamage();
+        int newDurability = (int) Math.round(currentDurability * multiplier);
+
+        if(newDurability > maximumDurability) {
+            newDurability = maximumDurability;
+        } else if(newDurability < 1) {
+            newDurability = 1;
+        }
+
+        int newDamage = maximumDurability - newDurability;
+        damageableItemMeta.setDamage(newDamage);
+        itemStack.setItemMeta((ItemMeta) damageableItemMeta);
+
+        return itemStack;
+    }
+
+    /**
+     *
+     * @param xp
+     * @param orbSize
+     * @param world
+     * @param loc
+     */
+    private void spawnOrbs(int xp, int orbSize, World world, Location loc) {
         while(xp > 0) {
-            Location loc = player.getLocation().add(Math.random() - 0.5, Math.random(), Math.random() - 0.5);
-            ExperienceOrb orb = (ExperienceOrb) player.getWorld().spawnEntity(loc, EntityType.EXPERIENCE_ORB);
+            ExperienceOrb orb = (ExperienceOrb) world.spawnEntity(loc, EntityType.EXPERIENCE_ORB);
             orb.setExperience(Math.min(orbSize, xp));
             xp = xp - orbSize;
         }
