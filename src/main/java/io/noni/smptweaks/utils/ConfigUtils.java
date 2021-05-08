577d55d64b28f5c7749cc21e0b5b8eb3954e4450
Nils-Erik Schulz (noniDOTio) <noni-online@outlook.de>
2021-05-08 18:35:35 +0200

    Accept utf-8 characters in language files and fix prevent NullPointerException

--- src/main/java/io/noni/smptweaks/utils/ConfigUtils.java
+++ src/main/java/io/noni/smptweaks/utils/ConfigUtils.java
@@ -3,6 +3,7 @@ package io.noni.smptweaks.utils;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,16 +27,25 @@ public class ConfigUtils {
     }
 
     /**
-     *
+     * Parse one dimensional yaml file
      * @param inputStream
-     * @return
+     * @return map with key value string pairs
      * @throws IOException
      */
     public static Map<String, String> parseSimpleConfig(@NotNull InputStream inputStream) throws IOException {
         HashMap<String, String> map = new HashMap<>();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         while(reader.ready()) {
             String line = reader.readLine();
+            if(line == null ) continue;
+
+            // Remove comments
+            line = line.replaceAll("#.*$", "");
+
+            // Skip line if there is no way it can contain a sane key value pair
+            if(line.length() < 2) continue;
+
+            // Parse line
             String[] fields = line.split(":", 2);
             map.put(fields[0], prepareValue(fields[1] != null ? fields[1] : ""));
         }
