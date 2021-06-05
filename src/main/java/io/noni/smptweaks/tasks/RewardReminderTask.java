4e930b9a0390566f9f4c9cd2f31ad89db97d2afa
Nils-Erik Schulz (noniDOTio) <noni-online@outlook.de>
2021-06-05 22:34:37 +0200

    Remind players their login reward is available when they join

---
+++ src/main/java/io/noni/smptweaks/tasks/RewardReminderTask.java
@@ -0,0 +1,29 @@
+package io.noni.smptweaks.tasks;
+
+import io.noni.smptweaks.SMPTweaks;
+import io.noni.smptweaks.utils.ChatUtils;
+import io.noni.smptweaks.utils.TranslationUtils;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Date;
+
+public class RewardReminderTask extends BukkitRunnable {
+    private Player player;
+
+    public RewardReminderTask(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public void run() {
+        Date lastRewardClaimedDatetime = SMPTweaks.getDB().getLastRewardClaimedDate(player);
+        int cooldownBetweenRewards = SMPTweaks.getCfg().getInt("rewards.cooldown");
+        int secondsSince = (int) (new Date().getTime() - lastRewardClaimedDatetime.getTime()) / 1000;
+        int secondsToWait = cooldownBetweenRewards - secondsSince;
+
+        if(secondsToWait < 0) {
+            ChatUtils.commandResponse(player, TranslationUtils.get("reward-available"));
+        }
+    }
+}
